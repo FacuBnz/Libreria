a21cf41caa9eed14f3a4287ab932d96f97848441
FacuBnz <facu.j.benitez@gmail.com>
2021-11-04 21:22:14 -0300

    se agregó la query update para modificar un autor

--- src/main/java/com/libreria/libreria/repository/AutorRepository.java
+++ src/main/java/com/libreria/libreria/repository/AutorRepository.java
@@ -2,6 +2,7 @@ package com.libreria.libreria.repository;
 
 import com.libreria.libreria.entity.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,8 @@ public interface AutorRepository extends JpaRepository<Autor, String>{
 
     @Query("SELECT a FROM Autor a WHERE a.id = :id")
     Autor findByIdAutor(@Param("id") String id);
+
+    @Modifying
+    @Query("UPDATE Autor a SET a.nombre = :nombre WHERE a.id = :id")
+    void modificar(@Param("nombre") String nombre, @Param("id") String id);
 }
